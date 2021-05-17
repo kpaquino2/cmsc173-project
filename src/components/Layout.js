9e911aab6c9a1477a8dd95a3ec2b092880833483
Kyle Aquino <kpaquino2@up.edu.ph>
2021-05-17 21:51:05 +0800

    feat: basic layout

---
+++ src/components/Layout.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/Layout.css"
+
+const Layout = () => {
+  return (
+    <div>
+      <div className="sidebar">
+
+      </div>
+      <div className="main">
+        
+      </div>
+      <div className="bottombar">
+      </div>
+    </div>
+  )
+}
+
+export default Layout;
