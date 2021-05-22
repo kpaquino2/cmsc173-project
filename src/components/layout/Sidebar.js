ab49d03c738b227d220707d1c1c8c65d44ae57ff
Rene Jotham Culaway <rcculaway@up.edu.ph>
2021-05-22 19:33:10 +0800

    feat: Added layout for sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,9 +1,20 @@
 import React from "react";
-import "../../styles/Layout.css";
+import "../../styles/Sidebar.css";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
+      <div className="sidebar-item title">
+          <h1>
+            Scheduler
+          </h1>
+      </div>
+      <div className="sidebar-item subjects-list">
+        test
+      </div>
+      <div className="sidebar-item options">
+        test
+      </div>
     </div>
   );
 }
