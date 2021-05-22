22c1d385a1e667df928d764e008378082d86cd9f
johnmelr <jrramos7@up.edu.ph>
2021-05-22 21:38:27 +0800

    feat: added subject card feature to side bar

---
+++ src/components/layout/Subject.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../../styles/Subject.css'
+
+const Subject = ({courseName, courseSection, startTime, endTime, daysOccur}) => {
+  return (
+    <>
+      <div className="subject-container" >
+        <div className="subject-text"> 
+          <h2>{courseName}</h2>
+        </div>
+        <div className="section-text">
+          <strong className="section-label">Section:</strong>
+          <span>{` ${courseSection}`}</span>
+        </div>
+        <div className="time-text">
+          <strong className="time-label">Time and Day:</strong>
+          <span>{` ${startTime}-${endTime} `}</span>
+          { daysOccur && daysOccur.map((day) => (
+            <span>{day}</span>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Subject;
