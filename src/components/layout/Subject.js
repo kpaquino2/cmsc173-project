c22ef26c75467e0d52d37070e70c8b84f1ab21f7
John Mel Ramos <jongmel@pop-os.localdomain>
2021-05-23 01:26:45 +0800

    feat: Added add lab feature

--- src/components/layout/Subject.js
+++ src/components/layout/Subject.js
@@ -1,10 +1,17 @@
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import '../../styles/Subject.css'
 
-const Subject = ({courseName, courseSection, startTime, endTime, daysOccur, bgColorVar}) => {
+const Subject = ({courseName, courseSection, startTime, endTime, daysOccur, bgColor}) => {
+  const labSections = [
+    {labSec: "ST1L", labStartTime: "7:00", labEndTime: "10:00", day:"M"},
+    {labSec: "ST2L", labStartTime: "10:00", labEndTime: "1:00", day:"M"}
+  ];
+
   return (
     <>
-      <div className="subject-container" styles={{bgColor: {bgColorVar}}}>
+      <div className="subject-container" style={{backgroundColor: {bgColor}}}>
         <div className="subject-text"> 
           <h2>{courseName}</h2>
         </div>
@@ -18,6 +25,32 @@ const Subject = ({courseName, courseSection, startTime, endTime, daysOccur, bgCo
           { daysOccur && daysOccur.map((day) => (
             <span>{day}</span>
           ))}
+        </div>
+        <div className="add-lab-container">
+          <button className="add-lab-button" >
+            <FontAwesomeIcon icon={faPlus} className="plus-icon" />
+            Add Lab
+          </button>
+        </div>
+        <div className="all-lab-sect-container">
+          {
+            labSections && labSections.map((labSection) => (
+              <div key={`${courseName}-${labSection}`} className="lab-section-container">
+                <div className="lab-section-text">
+                  <span>Lab Section:</span> 
+                  <span>{` ${labSection.labSec}`}</span>
+                </div>
+                <div className="lab-time-text">
+                  <span>Time:</span> 
+                  <span>{` ${labSection.labStartTime}-${labSection.labEndTime}`}</span>
+                </div>
+                <div className="lab-day-text">
+                  <span>Day:</span> 
+                  <span>{` ${labSection.day}`}</span>
+                </div>
+              </div>
+            ))
+          }
         </div>
       </div>
     </>
