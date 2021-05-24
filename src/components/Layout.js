6ccbcbd2e59dd4c50b9a9adbc58172665c749f27
Rene Jotham Culaway <rcculaway@up.edu.ph>
2021-05-24 20:33:11 +0800

    feat: Added non-functional drag-and-drop gestures to app

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,15 +3,23 @@ import Sidebar from "./layout/Sidebar";
 import Bottombar from "./bottombar/Bottombar";
 import Main from "./layout/Main";
 import "../styles/Layout.css"
+import { DndContext } from "@dnd-kit/core";
+import { restrictToWindowEdges } from "@dnd-kit/modifiers";
+
+const handleDragEnd = (event) => {
+  return;
+}
 
 const Layout = () => {
   return (
     <div className="outside-container">
-      <Sidebar />
-      <div className="schedule-panel">
-        <Main />
-        <Bottombar />
-      </div>
+      <DndContext modifiers={{restrictToWindowEdges}} onDragEnd={handleDragEnd}>
+        <Sidebar />
+        <div className="schedule-panel">
+          <Main />
+          <Bottombar />
+        </div>
+      </DndContext>
     </div>
   )
 }
