c6d4bafeafe0fcc665c9f4f0286925ae3c7a1b92
Kyle Aquino <kpaquino2@up.edu.ph>
2021-05-28 00:01:51 +0800

    chore: add text to add a plan tab

--- src/components/bottombar/Bottombar.js
+++ src/components/bottombar/Bottombar.js
@@ -2,12 +2,10 @@ import React from "react";
 import Tab from "./Tab";
 import { useAtom } from "jotai";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPlus,
-} from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import "../../styles/Layout.css";
 
-import { AboutUsModal } from "../layout/AboutUsModal"
+import { AboutUsModal } from "../layout/AboutUsModal";
 import { currentPlanAtom, plansAtom } from "../atom/plans";
 import { isOpenAtom } from "../atom/aboutusmodal";
 
@@ -21,55 +19,66 @@ const Bottombar = () => {
       <div
         className="tab"
         onClick={() => {
-          setPlans([...plans, {
-            number: plans[plans.length-1].number+1,
-            schedule: [
-              {
-                name: "Monday",
-                classes: [],
-              },
-              {
-                name: "Tuesday",
-                classes: [],
-              },
-              {
-                name: "Wednesday",
-                classes: [],
-              },
-              {
-                name: "Thursday",
-                classes: [],
-              },
-              {
-                name: "Friday",
-                classes: [],
-              },
-              {
-                name: "Saturday",
-                classes: [],
-              },
-            ]
-          }]);
+          setPlans([
+            ...plans,
+            {
+              number: plans[plans.length - 1].number + 1,
+              schedule: [
+                {
+                  name: "Monday",
+                  classes: [],
+                },
+                {
+                  name: "Tuesday",
+                  classes: [],
+                },
+                {
+                  name: "Wednesday",
+                  classes: [],
+                },
+                {
+                  name: "Thursday",
+                  classes: [],
+                },
+                {
+                  name: "Friday",
+                  classes: [],
+                },
+                {
+                  name: "Saturday",
+                  classes: [],
+                },
+              ],
+            },
+          ]);
         }}
-        style={{width: "50px"}}
+        style={{ width: "110px" }}
       >
         <FontAwesomeIcon icon={faPlus} size="xs" />
+        <span style={{ padding: "0.5rem" }}>Add a Plan</span>
       </div>
       <div className="plans">
         {plans.map((plan, idx) => {
           return (
-            <Tab key={idx} plan={plan} current={plan.number === currentPlan.number ? true : false} />
+            <Tab
+              key={idx}
+              plan={plan}
+              current={plan.number === currentPlan.number ? true : false}
+            />
           );
         })}
       </div>
       <div className="about-us-area">
         <AboutUsModal />
-        <button className="about-us-button tab tab-not-current" onClick={() => setIsOpen(true)}>
+        <button
+          className="about-us-button tab tab-not-current"
+          onClick={() => setIsOpen(true)}
+        >
           About Us
         </button>
       </div>
     </div>
   );
-}
+};
 
 export default Bottombar;
